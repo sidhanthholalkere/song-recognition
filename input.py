4fbfc259a4df73880f3efd5ea1f789cdb79887a3
sidhanthholalkere <sidhanth.holalkere@gmail.com>
2020-07-09 16:34:46 -0400

    added functionality to record from a mic

--- input.py
+++ input.py
@@ -1,5 +1,7 @@
 import librosa
+import numpy as np
 from pathlib import Path
+from microphone import record_audio
 
 SAMPLING_RATE = 44100
 
@@ -20,5 +22,25 @@ def mp3_to_samples(path):
     path = Path(path)
     
     recorded_audio, _ = librosa.load(path, sr=SAMPLING_RATE, mono=True)
+
+    return recorded_audio
+
+def mic_to_samples(time):
+    """Converts audio from micropgone input into samples
+
+    Parameters
+    ----------
+    time : int
+        The duration the microphone should record for.
+
+    Returns
+    -------
+    recorded_audio : np.array
+        The audio samples from the microphone input
+    """
+
+    frames, sample_rate = record_audio(time)
+
+    recorded_audio = np.hstack([np.frombuffer(i, np.int16) for i in frames])
 
     return recorded_audio
