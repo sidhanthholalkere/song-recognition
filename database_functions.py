734923df6244869e3fe4eb7295980c412c61a22f
Olivia Ma <olivia.ma@myxing.net>
2020-07-09 16:52:49 -0400

    updated storage function

--- database_functions.py
+++ database_functions.py
@@ -3,8 +3,17 @@
 song_database = dict()
 
 
-def store_fingerprint(fanout_m, song_id):
-    for (fm, fn, dt), tm in fanout_m:
+def store_fingerprint(fingerprints, song_id):
+    """
+        takes a list of fingerprints of a song and the song's ID, adds the prints and ID to database
+        Parameters
+        ----------
+            fingerprints : List[Tuple(float, float, float)]
+                (freq of initial peak, freq of peak fanned out to, time gap between peaks
+            song_id : str
+                ID of song the fingerprints are taken from
+    """
+    for (fm, fn, dt), tm in fingerprints:
         if (fm, fn, dt) not in song_database:
             song_database[(fm, fn, dt)] = [(song_id, tm)]
         else:
@@ -12,4 +21,6 @@ def store_fingerprint(fanout_m, song_id):
 
 
 def query_fingerprint(fingerprint):
+    if fingerprint in song_database:
+
     pass
