864f3f033f56d05ae6d1a175d6df83a0ad680652
CrAZYGreenAlpaca <39609875+CrAZYGreenAlpaca@users.noreply.github.com>
2020-07-09 13:00:26 -0700

    just added file

---
+++ audio_to_spectrogram.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+
+
+def audio_to_spectogram():
+    return()
+
+    """
+    Parameters
+    ----------
+    
+    Returns
+    -------
+    
+    
+    Examples
+    --------
+    
+    
+    """
