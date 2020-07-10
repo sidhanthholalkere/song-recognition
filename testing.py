6a6c24c65f05ad2a7adae29b3293df918064bb17
CrAZYGreenAlpaca <39609875+CrAZYGreenAlpaca@users.noreply.github.com>
2020-07-10 11:20:35 -0700

    comments and clean up

--- testing.py
+++ testing.py
@@ -84,37 +84,6 @@ def test_mp3(path, random_noise=False, random_slice=False):
     
     """
 
-def fourier_complex_to_real(complex_coeffs, N):
-    """
-    Converts complex-valued Fourier coefficients (of 
-    real-valued data) to the associated amplitudes and 
-    phase-shifts of the real-valued sinusoids
-    
-    Parameters
-    ----------
-    complex_coeffs : numpy.ndarray, shape-(N//2 + 1,)
-        The complex valued Fourier coefficients for k=0, 1, ...
-    
-    N : int
-        The number of samples that the DFT was performed on.
-    
-    Returns
-    -------
-    Tuple[numpy.ndarray, numpy.ndarray]
-        (amplitudes, phase-shifts)
-        Two real-valued, shape-(N//2 + 1,) arrays
-    """
-    amplitudes = np.abs(complex_coeffs) / N
-
-    # |a_k| = 2 |c_k| / N for all k except for
-    # k=0 and k=N/2 (only if N is even)
-    # where |a_k| = |c_k| / N
-    amplitudes[1 : (-1 if N % 2 == 0 else None)] *= 2
-
-    phases = np.arctan2(-complex_coeffs.imag, complex_coeffs.real)
-    return amplitudes, phases
-
-
 def test_from_mic(time, random_noise, random_slice):
 
     """
