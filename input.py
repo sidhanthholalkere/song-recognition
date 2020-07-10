fixed scaling

input.py
@@ -23,10 +23,8 @@ def mp3_to_samples(path):
     path = Path(path)
     
     recorded_audio, _ = librosa.load(path, sr=SAMPLING_RATE, mono=True)
-
-    scaler = MinMaxScaler(feature_range=(-2**15, 2**15))
     
-    return scaler.fit_transform(recorded_audio.reshape(-1, 1))
+    return recorded_audio * 2**15
 
 def mic_to_samples(time):
     """Converts audio from micropgone input into samples
@@ -46,6 +44,4 @@ def mic_to_samples(time):
 
     recorded_audio = np.hstack([np.frombuffer(i, np.int16) for i in frames])
 
-    scaler = MinMaxScaler(feature_range=(-2**15, 2**15))
-
-    return scaler.fit_transform(recorded_audio.reshape(-1, 1))
+    return recorded_audio