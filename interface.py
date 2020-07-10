28ac3b7fa731e23ad7ee94135f9a21db0f22023b
Olivia Ma <olivia.ma@myxing.net>
2020-07-10 12:35:41 -0400

    commented revised interface structure

--- interface.py
+++ interface.py
@@ -35,8 +35,27 @@ elif user_choice == 2:
     database.SongDatabase.query_fingerprint(fingerprints)
 
 
+# revised interface:
+'''
+print("Add to song database or try to match song?")
+print("1: Add to song database")
+user_choice = input("2: Match Song")
+if user_choice == 1:
+    song_name = input("Enter the song name")
+print("Use the mic or mp3")
+print("1: Use mic")
+method = input("2: Use mp3")
 
-
+if user_choice == 1: (store song)
+    pass song_name and method to database.store_song
+    database stores song_name, does lines 24-30 to generate fingerprints
+    stores the generated fingerprints
+else: (query song)
+    pass method to database.query_song
+    database does lines 24-30 to generate the fingerprint
+    checks database for song with highest tally
+    returns song name with highest tally
+'''
 
 
 
