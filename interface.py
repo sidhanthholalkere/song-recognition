4b8646bfa1350a588598b29890e3f7d42cf5b937
Austin Yang <austinqyang@gmail.com>
2020-07-10 08:46:33 -0700

    fixed functions

--- interface.py
+++ interface.py
@@ -2,6 +2,7 @@ from scipy.ndimage.filters import maximum_filter
 from scipy.ndimage.morphology import generate_binary_structure, binary_erosion
 from scipy.ndimage.morphology import iterate_structure
 import utils
+import database
 
 print("Add to song database or try to match song?")
 print("1: Add to song database")
@@ -15,23 +16,23 @@ method = input("2: Use mp3")
 
 if (method == 1):
     time = input("Enter the length of the song")
-    audio = #(what we name the input file).mic_to_samples(time)
+    audio = utils.mic_to_samples(time)
 else:
     path = input("Enter the path of the mp3 file")
-    audio = #(what we name the input file).mp3_to_samples(path)
+    audio = utils.mp3_to_samples(path)
 
-spectogram = audio_to_spectrogram(44100, audio)
-threshhold = threshhold_value(spectogram)
+spectogram = utils.audio_to_spectrogram(44100, audio)
+threshhold = utils.threshhold_value(spectogram)
 temp_neighborhood = generate_binary_structure(2, 1)
 neighborhood = iterate_structure(temp_neighborhood, 3)
-local_peaks = local_peak_locations(spectogram, )
+local_peaks = utils.local_peak_locations(spectogram, )
 fanout_num=15
-fingerprints = fingerprint(local_peaks, fanout_num)
+fingerprints = utils.fingerprint(local_peaks, fanout_num)
 
 if user_choice == 1:
-    SongDatabase.store_fingerprint(fingerprints, song_name)
+    database.SongDatabase.store_fingerprint(fingerprints, song_name)
 elif user_choice == 2:
-    SongDatabase.query_fingerprint(fingerprints)
+    database.SongDatabase.query_fingerprint(fingerprints)
 
 
 
